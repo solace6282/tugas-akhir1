2754bd03da8a3ebcd2664d5126b9c7b7bd85fd52
solace6282 <gkataro4@gmail.com>
2023-06-26 16:41:19 +0700

    og data

--- BARTABSA/peng/grid.py
+++ BARTABSA/peng/grid.py
@@ -1,9 +1,36 @@
+
+from data.pipe import BartBPEABSAPipe
 from model.modeling_bart import BartModel
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, BartTokenizer
 from sklearn.model_selection import GridSearchCV
 
-tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
-tokens = tokenizer.batch_encode_plus(texts, padding=True, truncation=True, return_tensors='pt')
+def get_data():
+    pipe = BartBPEABSAPipe(tokenizer='facebook/bart-base', opinion_first=True, dataset = f'../final_data/opener_en/')
+    data_bundle = pipe.process_from_file(f'../final_data/opener_en/', demo=False)
+    return data_bundle, pipe.tokenizer, pipe.mapping2id
+
+data_bundle, tokenizer, mapping2id = get_data()
+model = BartModel.from_pretrained('facebook/bart-base')
+num_tokens, _ = model.encoder.embed_tokens.weight.shape
+model.resize_token_embeddings(len(tokenizer.unique_no_split_tokens)+num_tokens)
+encoder = model.encoder
+decoder = model.decoder
+
+_tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+for token in tokenizer.unique_no_split_tokens:
+    if token[:2] == '<<':
+        index = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(token))
+        if len(index)>1:
+            raise RuntimeError(f"{token} wrong split")
+        else:
+            index = index[0]
+        assert index>=num_tokens, (index, num_tokens, token)
+        indexes = _tokenizer.convert_tokens_to_ids(_tokenizer.tokenize(token[2:-2]))
+        embed = model.encoder.embed_tokens.weight.data[indexes[0]]
+        for i in indexes[1:]:
+            embed += model.decoder.embed_tokens.weight.data[i]
+        embed /= len(indexes)
+        model.decoder.embed_tokens.weight.data[index] = embed
 
 def bart_model_fn(params):
     model = BartModel.from_pretrained('facebook/bart-base', **params)
@@ -18,7 +45,8 @@ param_grid = {
 
 grid_search = GridSearchCV(estimator=bart_model_fn, param_grid=param_grid)
 
-grid_search.fit(tokens['input_ids'], tokens['attention_mask'])
+print(data_bundle.get_dataset('train'))
+grid_search.fit(data_bundle.get_dataset('train')['src_tokens'], data_bundle.get_dataset('train')['aspects'])
 
 best_model = grid_search.best_estimator_
 best_params = grid_search.best_params_
